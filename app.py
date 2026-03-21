508d9e4f4b14cf40ddbbb1f897612141e1f9ad90
Annu9111 <aksoni911190@gmail.com>
2026-03-21 15:09:42 +0530

    strick

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
-
+from datetime import datetime
 app = Flask(__name__)
 
 def get_db():
@@ -20,12 +20,26 @@ def dashboard():
     cursor.execute("SELECT * FROM study ORDER BY date DESC")
     data = cursor.fetchall()
 
-    # Calculate total time
     total_time = sum(row[2] for row in data)
+
+    #  Streak logic
+    dates = list(set(row[3][:10] for row in data))  # get unique dates
+    dates.sort(reverse=True)
+
+    streak = 0
+    today = datetime.now().date()
+
+    for i, d in enumerate(dates):
+        d = datetime.strptime(d, "%Y-%m-%d").date()
+        if (today - d).days == i:
+            streak += 1
+        else:
+            break
 
     conn.close()
 
-    return render_template("dashboard.html", data=data, total_time=total_time)
+    return render_template("dashboard.html", data=data,
+                           total_time=total_time, streak=streak)
 
 
 @app.route('/add', methods=['POST'])
