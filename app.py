dbdb1460a77b35bc23e6786cb2fa08a4a84b543b
Annu9111 <aksoni911190@gmail.com>
2026-03-21 13:05:59 +0530

    update app

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db():
+    return sqlite3.connect('database/database.db')
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/add', methods=['POST'])
+def add():
+    subject = request.form['subject']
+    time = request.form['time']
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO study (subject, time) VALUES (?, ?)", (subject, time))
+
+    conn.commit()
+    conn.close()
+
+    return redirect('/dashboard')
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
